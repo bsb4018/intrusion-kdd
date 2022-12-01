da05ccf4e08937e2cf6b903c02d4909cc6a5705c
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-12-01 22:00:25 +0530

    Data Ingestion Work

---
+++ kddid/constant/training/data_ingestion.py
@@ -0,0 +1,6 @@
+DATA_INGESTION_DIR = "data_ingestion"
+DATA_INGESTION_DOWNLOADED_DATA_DIR = "downloaded_files"
+DATA_INGESTION_FILE_NAME = "customer_complaint"
+DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
+DATA_INGESTION_FAILED_DIR = "failed_downloaded_files"
+DATA_INGESTION_METADATA_FILE_NAME = "meta_info.yaml"
